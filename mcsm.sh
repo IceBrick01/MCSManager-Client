c29bd680a845f98e51bbd04d1950472778324fd9
IceBrick <91176400+IceBrick01@users.noreply.github.com>
2022-03-14 17:35:22 +0800

    Update mcsm.sh

--- mcsm.sh
+++ mcsm.sh
@@ -6,21 +6,21 @@ Red_Error() {
 }
 
 echo "============= MCSManager Client ===============
-(1) 重啟面版服務       (8) 重启守护进程
-(2) 停止面版服务       (9) 停止守护进程
-(3) 启动面版服务       (10) 启动守护进程
-(4) 禁用面版服务       (11) 禁用守护进程
-(5) 启用面版服务       (12) 启用守护进程
+(1) 重啟面版服務       (8) 重啟守護進程
+(2) 停止面版服務       (9) 停止守護進程
+(3) 啟動面版服務       (10) 启动守護進程
+(4) 禁用面版服務       (11) 禁用守護進程
+(5) 啟用面版服務       (12) 啟用守護進程
 (6) 修改管理密碼       (13) 清理面版日志
-(7) 卸载管理面版       (14) 全部重啟
+(7) 卸載管理面版       (14) 全部重啟
 (0) 退出
 ==============================================="
 
-read -r -p "[-] 请输入命令编号: " cmd;
+read -r -p "[-] 請输入命令代號: " cmd;
 
 if [ "$cmd" ] && [ "$cmd" -gt 0 ] && [ "$cmd" -lt 15 ]; then
    echo "==============================================="
-   echo "[-] 正在执行($cmd)..."
+   echo "[-] 正在執行($cmd)..."
    echo "==============================================="
 fi
 
@@ -41,61 +41,61 @@ then
   systemctl enable mcsm-web.service
 elif [ "$cmd" == 6 ]
 then
-  read -r -p "[+] 请输入新密码: " new1;
+  read -r -p "[+] 請輸入新密碼: " new1;
 
   if [ "${#new1}" -lt 6 ]; then
     echo "==============================================="
-    echo "[x] 密码长度不能小于 6"
+    echo "[x] 密碼長度不能小於 6"
     exit
   fi
 
-  read -r -p "[+] 请再次输入新密码: " new2;
+  read -r -p "[+] 請再次輸入新密碼: " new2;
 
   if [ "$new1" != "$new2" ]; then
     echo "==============================================="
-    echo "[x] 两次输入的密码不一致"
+    echo "[x] 兩次輸入的密碼不一致"
     exit
   fi
 
-  echo "[-] 修改 MCSManager-Web root 密码..."
+  echo "[-] 修改 MCSManager-Web root 密碼..."
   passWord_old=$(awk -F"\"" '/passWord/{print $4}' /opt/mcsmanager/web/data/User/root.json)
   passWord_new=$(echo -n "$new2" | md5sum | cut -d ' ' -f1)
   sed -e "s@$passWord_old@$passWord_new@g" -i /opt/mcsmanager/web/data/User/root.json
 
-  echo "[-] 重启 MCSManager-Web 服务..."
+  echo "[-] 重启 MCSManager-Web 服務..."
   systemctl restart mcsm-web.service
 
-  echo "[+] root 密码已更新！"
+  echo "[+] root 密碼已更新！"
 elif [ "$cmd" == 7 ]
 then
-  Red_Error "[!] 卸载后无法找回数据，请先备份必要数据！"
-  read -r -p "[-] 确认已了解以上内容，我确定已备份完成 (输入yes继续卸载): " yes;
+  Red_Error "[!] 卸載後無法找回數據，請先備份必要數據！"
+  read -r -p "[-] 確認已了解以上内容，我確定已備份完成 (輸入yes繼續卸載): " yes;
   if [ "$yes" != "yes" ]; then
     echo "==============================================="
     echo "已取消！"
     exit
   fi
 
-  echo "[-] MCSManager 服务正在运行，停止服务..."
+  echo "[-] MCSManager 服務正在運行，停止服務..."
   systemctl stop mcsm-{daemon,web}.service
   systemctl disable mcsm-{daemon,web}.service
 
-  echo "[x] 删除 MCSManager 服务"
+  echo "[x] 删除 MCSManager 服務"
   rm -f /etc/systemd/system/mcsm-daemon.service
   rm -f /etc/systemd/system/mcsm-web.service
 
-  echo "[-] 重载服务配置文件"
+  echo "[-] 重載服務配置文件"
   systemctl daemon-reload
 
-  echo "[x] 删除 MCSManager 相关文件"
+  echo "[x] 删除 MCSManager 相關文件"
   rm -irf /opt/mcsmanager
 
-  echo "[x] 删除 MCSManager-命令行 相关文件"
+  echo "[x] 删除 MCSManager Client 相關文件"
   rm -f /usr/local/bin/mcsm
   rm -f /opt/mcsm.sh
 
   echo "==============================================="
-  echo -e "\033[1;32m卸载完成，感谢使用 MCSManager！\033[0m"
+  echo -e "\033[1;32m卸載完成，感謝使用 MCSManager！\033[0m"
 
 elif [ "$cmd" == 8 ]
 then
@@ -116,7 +116,7 @@ elif [ "$cmd" == 13 ]
 then
   rm -ifr /opt/mcsmanager/web/logs
   mkdir -p /opt/mcsmanager/web/logs
-  echo "[-] 已清空日志！"
+  echo "[-] 已清空日誌！"
 elif [ "$cmd" == 14 ]
 then
   systemctl restart mcsm-{daemon,web}.service
